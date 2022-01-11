52520beeab6b5bba51a886220f0803238c6603b0
Andre D'Souza <adsouz03@uoguelph.ca>
2022-01-11 18:17:30 -0500

    Added fork(), wait(), and execve() commands to execute user input. Needs to be refactored as child proceses do not terminate

--- src/main.c
+++ src/main.c
@@ -7,8 +7,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pwd.h>
-#include <unistd.h>
+#include <unistd.h>                         // For uid_t
 #include <sys/types.h>
+#include <sys/wait.h>
 
 //Local Libraries
 #include "FuncSet1.h"
@@ -21,9 +22,9 @@ int main ()
   int j = 0;
 
 
-  uid_t uid=getuid(), euid=geteuid(); // User Information Variables
-  char *userName;                     // User ID string
-  struct passwd *p;                   // User Infromation Struct
+  uid_t uid=getuid(), euid=geteuid();       // User Information Variables
+  char *userName;                           // User ID string
+  struct passwd *p;                         // User Infromation Struct
 
   //User Name Check
   if (((p = getpwuid(uid)) == NULL) ) {
@@ -33,32 +34,42 @@ int main ()
   } else {
     userName = strdup(p->pw_name);
   }
-  
-  //char *envp[] = {(char *) "PATH=/bin", 0}; //Environment Variables Commands /bin
+
+  char *envp[] = {(char *) "PATH=/bin", 0}; //Environment Variables Commands /bin
 
   while(1) { //Repeat Forever
     j=0;
 
-    //Display User Shell Prompt for Command
+    // Display User Shell Prompt for Command
     displayShell(uid, euid, userName);
-    //Read Input from Terminal
-    shellInput(command, arguments);
-    //Fork
-      //Parent
-        //Wait for Child
-    //Execute Command
 
-    //Exit Command
+    // Read Input from Terminal
+    shellInput(command, arguments);
+
+    //Fork
+    if (fork() != 0) {                      // Check if Parent has a Child Process
+      printf("Waiting for Child\n");
+      wait(NULL);                           // Wait for Chiild
+    } else {
+      printf("Attempting to Execute Command = %s\n", command);
+      strcpy(cmd, "/bin/");                 // Command Base Directory
+      strcat(cmd, command);                 // Concatenate command to directory string
+      execve(cmd, arguments, envp);         // Execute Command
+    }
+
+    // Clear Command
+    if (strcmp(command, "clear") == 0) {
+      clear();
+    }
+    // Exit Command
+    else if (strcmp(command, "exit") == 0) {
+      exit(0);
+    }
+
     printf("Command = %s\n", command);
     while (arguments[j] != NULL) {
       printf("Argument[%d] = %s\n", j, arguments[j]);
       j++;
-    }
-    if (strcmp(command, "clear") == 0) {
-      clear();
-    }
-    if (strcmp(command, "exit") == 0) {
-      exit(0);
     }
   }
 
