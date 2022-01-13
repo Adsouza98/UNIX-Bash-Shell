68f4d453eecf29b2168586ed0ea3cd36909584ee
Andre D'Souza <adsouz03@uoguelph.ca>
2022-01-12 19:28:45 -0500

    Created testfile.c to test executing local files in shell. Program just prints then sleeps for 10s then prints again on exit

---
+++ src/testfile.c
@@ -0,0 +1,14 @@
+// Standard Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main()
+{
+  printf("Doing Some Extreme task\n");
+  sleep(10);
+  printf("Done Extreme task\n");
+
+  return 0;
+}
