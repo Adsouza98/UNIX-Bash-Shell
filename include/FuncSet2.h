6e8893f49b89a3fff2503572d63228421e04d17b
Andre D'Souza <adsouz03@uoguelph.ca>
2022-01-13 15:49:21 -0500

    Added outRedirToFile() to Redirect command output to file

--- include/FuncSet2.h
+++ include/FuncSet2.h
@@ -2,5 +2,6 @@
 #define FuncSet2
 
 void funcSet2Print();
+int outRedirToFile(char* command, char* arg[], char* fileName[]);
 
 #endif
--- src/funcSet2.c
+++ src/funcSet2.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 // Local Libraries
 #include "FuncSet2.h"
@@ -18,4 +19,27 @@
 void funcSet2Print()
 {
   printf("Hello FuncSet2!\n");
+}
+
+int outRedirToFile(char* command, char* arg[], char* fileName[])
+{
+  FILE* fp;
+  char cmd[50];
+  int status;
+
+  if (fileName != NULL) {
+    // Open File for writing from stdout
+    // w = if file does not exist create it, if it does exist rewrite file
+    fp = freopen(fileName[0], "w", stdout);
+
+    // Command is in PATH=/bin/
+    strcpy(cmd, "/bin/");
+    strcat(cmd, command);
+    status = execvp(cmd, arg);          // Execute in PATH=/bin Dir
+    fclose(fp);
+    return status;
+  } else {
+    perror("File Failed to Open\n");
+    return -1;
+  }
 }
